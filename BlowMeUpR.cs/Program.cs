7ddf7a0d63094366e1b8ddb1863a7534c8b4a126
danteCLRK <fryanslaucerers@gmail.com>
2017-07-21 11:24:50 -0400

    Finished

---
+++ BlowMeUpR.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_Blow_Me_up
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+
+            // board.GameBoard()
+            PlayGame newGame = new PlayGame();
+            int size = newGame.GetSize();
+            int bomb = newGame.GetBomb();
+            int[,] arr1 = new int[size + 2, size + 2];
+            arr1 = board.AddMines(arr1, bomb);
+            int[,] backBoard = board.GameBoard(arr1);
+            int[,] originalboard = arr1;
+            bool cont = true;
+
+            string[,] playerBoard = newGame.MakeBoard();
+            while (cont)
+            {
+                int[] choices = newGame.GetChoice();
+                playerBoard = newGame.UpdateBoard(backBoard, playerBoard, choices, bomb);
+
+                newGame.PrintBoard(playerBoard);
+
+                Console.WriteLine();
+                if (playerBoard[0, 0] == "1")
+                {
+                    cont = false;
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+            newGame.PrintFinalBoard(backBoard);
+        }
+    }
+}
